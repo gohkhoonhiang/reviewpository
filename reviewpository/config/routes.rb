5094c543c7e62a70cbc6f62d782e94cfff6fcd5d
Goh Khoon Hiang <gohkhoonhiang@gmail.com>
2016-07-31 18:07:37 +0800

    Create routes

--- reviewpository/config/routes.rb
+++ reviewpository/config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'items#search'
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  post 'logout', to: 'sessions#destroy'
+  get 'signup', to: 'users#new'
+  post 'signup', to: 'users#create'
+  get 'items/query', to: 'items#query'
+  get 'account/settings', to: 'users#edit'
+  get 'items/listing', to: 'items#index'
+
+  resources :users, except: [:edit, :new, :create]
+  resources :items, except: [:index]
+  resources :reviews
+  resources :comments
 end
