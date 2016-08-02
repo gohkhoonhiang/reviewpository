22c489e4c7e8c420ad8f56c27941b69d5a70d0ea
Goh Khoon Hiang <gohkhoonhiang@gmail.com>
2016-08-03 00:55:02 +0800

    Posting item

--- reviewpository/app/controllers/items_controller.rb
+++ reviewpository/app/controllers/items_controller.rb
@@ -1,8 +1,24 @@
 class ItemsController < ApplicationController
+  skip_before_action :require_login, :only => [:index, :search, :show]
+
   def index
+    @items = Item.all
   end
 
   def create
+    item = current_user.items.new
+    item.category = params[:category]
+    item.name = params[:name]
+    item.description = params[:description]
+    item.save
+    stakeholders = params[:stakeholder].split(",")
+    stakeholders.each do |s|
+      stakeholder = Stakeholder.new
+      stakeholder.name = s
+      stakeholder.item = item
+      stakeholder.save
+    end
+    redirect_to "/items/listing"
   end
 
   def new
@@ -24,5 +40,8 @@ class ItemsController < ApplicationController
   end
 
   def query
+  end
+
+  def user_items
   end
 end
