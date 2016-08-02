e855b5bc9424c4849cb18948861a13f08bb6febd
Goh Khoon Hiang <gohkhoonhiang@gmail.com>
2016-08-02 21:43:23 +0800

    Add header layout

--- reviewpository/app/controllers/application_controller.rb
+++ reviewpository/app/controllers/application_controller.rb
@@ -1,3 +1,19 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  helper_method :current_user, :logged_in?
+  before_filter :require_login
+
+  def current_user
+    @current_user ||= User.find_by(username: session[:username])
+  end
+
+  def logged_in?
+    current_user != nil
+  end
+
+  def require_login
+    unless current_user
+      redirect_to root_url
+    end
+  end
 end
