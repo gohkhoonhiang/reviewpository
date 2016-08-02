0e9ae5ee9381505441b609f48b6f1a69e5b536b3
Goh Khoon Hiang <gohkhoonhiang@gmail.com>
2016-08-02 21:57:00 +0800

    Login

--- reviewpository/app/controllers/sessions_controller.rb
+++ reviewpository/app/controllers/sessions_controller.rb
@@ -1,10 +1,25 @@
 class SessionsController < ApplicationController
+  skip_before_action :require_login, :only => [:new, :create]
+
   def new
   end
 
   def create
+    if params[:username] && params[:password]
+      user = User.find_by(username: params[:username])
+      if user && user.authenticate(params[:password])
+        session[:username] = params[:username]
+        redirect_to "/user/items"
+      else
+        render :new
+      end
+    else
+      render :new
+    end
   end
 
   def destroy
+    reset_session
+    redirect_to root_url
   end
 end
