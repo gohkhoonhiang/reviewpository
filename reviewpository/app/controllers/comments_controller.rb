d83e1a4997a75e412961e551a29dd0168454ddff
Goh Khoon Hiang <gohkhoonhiang@gmail.com>
2016-08-04 18:17:01 +0800

    Post comment

--- reviewpository/app/controllers/comments_controller.rb
+++ reviewpository/app/controllers/comments_controller.rb
@@ -6,6 +6,13 @@ class CommentsController < ApplicationController
   end
 
   def create
+    review = Review.find(params[:review_id])
+    item = review.item
+    comment = review.comments.new
+    comment.content = params[:content]
+    comment.user = current_user
+    comment.save
+    redirect_to item_path(item)
   end
 
   def edit
